eebbb732bcd4894b830757227793ead66755a5b6
Nova <aelithron@gmail.com>
2025-11-19 22:49:28 -0700

    start implementing search better!

--- app/select/select.module.tsx
+++ app/select/select.module.tsx
@@ -3,10 +3,10 @@ import { parseBlob } from "music-metadata";
 import { showOpenFilePicker } from "show-open-file-picker";
 import type { Song } from "@/lyrickit";
 import { db } from "@/utils/db";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { MusicBrainzApi } from "musicbrainz-api";
+import { IReleaseList, MusicBrainzApi } from "musicbrainz-api";
 
 export function UploadSongs() {
   async function processFiles() {
@@ -51,17 +51,18 @@ export function UploadSongs() {
   return <button type="button" onClick={processFiles} className="p-2 rounded-lg bg-violet-300 text-black mt-2 hover:text-sky-500"><FontAwesomeIcon icon={faMusic} /> Select Songs</button>
 }
 
-export function SearchSongs() {
+export function SearchSongs({ setSearchedSongs }: { setSearchedSongs: Dispatch<SetStateAction<IReleaseList[]>> }) {
   const [title, setTitle] = useState<string>("");
   const [artist, setArtist] = useState<string>("");
-  const [album, setAlbum] = useState<string>("");
-  const mbApi = new MusicBrainzApi({
+  const mbAPI = new MusicBrainzApi({
     appName: 'LyricKit',
     appVersion: process.env.IMAGE_TAG || "Unknown",
     appContactInfo: 'https://github.com/aelithron/lyrickit',
   });
-  async function searchSongs() {
-    
+  async function searchSongs(e: React.FormEvent) {
+    e.preventDefault();
+    const results = await mbAPI.search("release", { query: `query=artist:"${artist}" AND release-group:"${title}"` });
+    setSearchedSongs(results.releases);
   }
   return (
     <form onSubmit={searchSongs} className="flex flex-col mt-2 gap-1">
@@ -69,9 +70,8 @@ export function SearchSongs() {
       <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} className="bg-slate-500 rounded-lg p-1" placeholder="Enter a title..." />
       <label htmlFor="artist">Artist</label>
       <input type="text" id="artist" value={artist} onChange={(e) => setArtist(e.target.value)} className="bg-slate-500 rounded-lg p-1" placeholder="Enter an artist..." />
-      <label htmlFor="album">Album</label>
-      <input type="text" id="album" value={album} onChange={(e) => setAlbum(e.target.value)} className="bg-slate-500 rounded-lg p-1" placeholder="Enter an album..." />
       <button type="submit" className="bg-violet-300 p-2 rounded-lg text-black mt-3"><FontAwesomeIcon icon={faSearch} /> Search</button>
     </form>
   )
 }
+function
---
+++ app/select/stateManager.module.tsx
@@ -0,0 +1,22 @@
+"use client";
+import type { IReleaseList } from "musicbrainz-api";
+import { useState } from "react";
+import { SearchSongs, UploadSongs } from "./select.module";
+
+export default function SelectStateManager() {
+  const [searchedSongs, setSearchedSongs] = useState<IReleaseList[]>([]);
+  return (
+    <div className="grid grid-rows-3 grid-cols-1 md:grid-cols-3 md:grid-rows-1 mt-6 md:col-span-2">
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">Upload Songs</h1>
+        <UploadSongs />
+      </div>
+      <p className="text-slate-700 dark:text-slate-300 text-center align-middle">OR</p>
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">Search for Songs</h1>
+        <SearchSongs setSearchedSongs={setSearchedSongs} />
+      </div>
+
+    </div>
+  )
+}
